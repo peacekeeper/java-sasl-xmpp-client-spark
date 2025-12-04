7d5e95883e3bd37c892d912a006ae4c1395fd8ff
Markus Sabadello <markus@danubetech.com>
2025-12-04 14:46:55 +0100

    Upgrade DID-CHALLENGE integration

--- src/main/java/sasl/xmpp/client/DIDChallengeSaslPlugin.java
+++ src/main/java/sasl/xmpp/client/DIDChallengeSaslPlugin.java
@@ -1,30 +1,19 @@
 package sasl.xmpp.client;
 
-import demo.sasl.client.SaslClientCallbackHandler;
-import demo.sasl.client.integration.UserIntegrationWithDID;
-import org.apache.commons.codec.binary.Hex;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jivesoftware.smack.ConnectionListener;
 import org.jivesoftware.smack.SASLAuthentication;
 import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.sasl.SASLMechanism;
-import org.jivesoftware.smack.sasl.javax.SASLJavaXMechanism;
-import org.jivesoftware.smack.sasl.javax.SmackJavaxSaslException;
 import org.jivesoftware.spark.SparkManager;
 import org.jivesoftware.spark.plugin.Invokable;
 import org.jivesoftware.spark.plugin.Plugin;
-import org.jivesoftware.sparkimpl.settings.local.SettingsManager;
 import sasl.mechanism.did.DIDChallengeSaslProvider;
 import sasl.xmpp.client.debug.SaslClientDebug;
 
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.sasl.Sasl;
-import javax.security.sasl.SaslException;
 import javax.swing.*;
 import java.security.Security;
 import java.util.Arrays;
-import java.util.Map;
 
 public class DIDChallengeSaslPlugin implements Plugin, Invokable, ConnectionListener {
 
@@ -38,18 +27,13 @@ public class DIDChallengeSaslPlugin implements Plugin, Invokable, ConnectionList
         SaslClientDebug.logSaslClientFactoriesAndMechanisms();
     }
 
-    static {
-        SASLAuthentication.registerSASLMechanism(new SASLDIDChallengeJavaXMechanism());
-        log.debug("SASL mechanisms: " + SASLAuthentication.getRegisterdSASLMechanisms());
-    }
-
-    static {
-        SparkManager.getConnection().addConnectionListener(new DIDChallengeSaslPlugin());
-    }
-
     @Override
     public void initialize() {
         log.debug("initialize");
+        SASLAuthentication.unregisterSASLMechanism(SASLDIDChallengeJavaXMechanism.class.getName());
+        SASLAuthentication.registerSASLMechanism(new SASLDIDChallengeJavaXMechanism(true));
+        log.debug("SASL mechanisms: " + SASLAuthentication.getRegisterdSASLMechanisms());
+        SparkManager.getConnection().addConnectionListener(this);
         JOptionPane.showMessageDialog(null, "DID-CHALLENGE SASL mechanism: initialize");
     }
 
@@ -83,62 +67,5 @@ public class DIDChallengeSaslPlugin implements Plugin, Invokable, ConnectionList
     public boolean invoke(Object... objects) {
         log.debug("invoke: " + Arrays.asList(objects));
         return false;
-    }
-
-    /*
-     * Helper classes
-     */
-
-    private static class SASLDIDChallengeJavaXMechanism extends SASLJavaXMechanism {
-
-        @Override
-        public String getName() {
-            return DIDChallengeSaslProvider.MECHANISM_NAME;
-        }
-
-        @Override
-        public int getPriority() {
-            return 0;
-        }
-
-        @Override
-        protected SASLMechanism newInstance() {
-            return new SASLDIDChallengeJavaXMechanism();
-        }
-
-        @Override
-        public boolean requiresPassword() {
-            return false;
-        }
-
-        @Override
-        protected void authenticateInternal() throws SmackJavaxSaslException {
-            String[] mechanisms = { getName() };
-            Map<String, String> props = getSaslProps();
-            String authzid = null;
-            if (authorizationId != null) {
-                authzid = authorizationId.toString();
-            }
-            try {
-                sc = Sasl.createSaslClient(mechanisms, authzid, "xmpp", getServerName().toString(), props,
-                        new SaslClientCallbackHandler(new UserIntegrationWithDID()));
-            }
-            catch (SaslException e) {
-                throw new SmackJavaxSaslException(e);
-            }
-        }
-
-        @Override
-        protected void authenticateInternal(CallbackHandler cbh) throws SmackJavaxSaslException {
-            super.authenticateInternal(cbh);
-            log.info("authenticateInternal " + cbh + " -> " + this.sc);
-        }
-
-        @Override
-        protected byte[] getAuthenticationText() throws SmackJavaxSaslException {
-            byte[] result = super.getAuthenticationText();
-            log.info("getAuthenticationText -> " + (result == null ? result : Hex.encodeHexString(result)));
-            return result;
-        }
     }
 }
